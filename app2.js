27f8782717d578d8e59b2ead75e919cd7fb0cda8
jorgegrajalesu <jegosystem@gmail.com>
2023-08-21 23:34:01 -0500

    fundamentos

--- app2.js
+++ app2.js
@@ -6,5 +6,10 @@ const fs = require('fs');
 
 const data = fs.readFileSync('readme1.md', 'utf8');
 
+// actualizar la data
+const nuevaInfo = data.replace(/React/ig, 'Angular');
+
+fs.writeFileSync('readme1-angular.md', nuevaInfo);
+
 console.log(data);
 
